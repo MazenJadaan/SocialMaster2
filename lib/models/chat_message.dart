345358867a04c0f702c8d320304c448a8355b021
Aya Al Babbily <yooosha000@gmail.com>
2023-07-05 13:21:41 +0300

    123 123

---
+++ lib/models/chat_message.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+import '../modules/chat_detail_page.dart';
+
+class ChatMessage{
+  String message;
+  MessageType type;
+  ChatMessage({required this.message,required this.type});
+}
